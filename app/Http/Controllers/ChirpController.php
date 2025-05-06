8272e94b9dfe5e72eaf9bf718121f5fa750a9ca9
s020080o <s020080o@student.staffs.ac.uk>
2025-05-06 18:44:13 +0100

    added edit button

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -6,6 +6,7 @@ use App\Models\Chirp;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Gate;
 
 use Illuminate\View\View;
 
@@ -14,11 +15,31 @@ class ChirpController extends Controller
     /**
      * Display a listing of the resource.
      */
+    public function edit(Chirp $chirp): View
+    {
+        Gate::authorize('update', $chirp);
+ 
+        return view('chirps.edit', [
+            'chirp' => $chirp,
+        ]);
+    }
     public function index(): View
     {
         return view('chirps.index', [
             'chirps' => Chirp::with('user')->latest()->get(),
         ]);
+    }
+    public function update(Request $request, Chirp $chirp): RedirectResponse
+    {
+        Gate::authorize('update', $chirp);
+ 
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+ 
+        $chirp->update($validated);
+ 
+        return redirect(route('chirps.index'));
     }
     public function store(Request $request): RedirectResponse
     {
