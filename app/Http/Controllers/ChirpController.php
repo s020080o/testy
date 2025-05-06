31d71dff2d0f988b21c59c37fc7ed6d7b2b1c7dd
s020080o <s020080o@student.staffs.ac.uk>
2025-05-06 16:35:54 +0100

    can send chirp and save chirp

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Chirp;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\Http\RedirectResponse;
 
 use Illuminate\View\View;
 
@@ -17,6 +18,18 @@ class ChirpController extends Controller
     {
         return view('chirps.index');
     }
+    public function store(Request $request): RedirectResponse
+    {
+         
+         $validated = $request->validate([
+             'message' => 'required|string|max:255',
+         ]);
+  
+         $request->user()->chirps()->create($validated);
+  
+         return redirect(route('chirps.index'));
+    }
+ 
 
 
 }
